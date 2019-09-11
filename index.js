15531118c80addb4822609f5f9d9220102b588dc
Gee-Bee <greg.bunia@gmail.com>
2019-09-11 13:07:27 +0200

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const morgan = require('morgan');
-app.use(morgan('tiny'));
+morgan.token('body', req => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/info', (req, res) => {
   res.send(`
