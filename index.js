1e7cc9e3c781793efa35a6f1f95c60465a1bba82
Gee-Bee <greg.bunia@gmail.com>
2019-09-11 11:08:52 +0200

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -20,9 +20,15 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
 })
 
-const persons = [
+let persons = [
   {
     "name": "Arto Hellas",
     "number": "040-123456",
