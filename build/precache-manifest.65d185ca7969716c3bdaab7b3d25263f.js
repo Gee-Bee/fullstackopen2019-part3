0dabf874927f832ada90b6065d2c6d49b3c2a47b
Gee-Bee <greg.bunia@gmail.com>
2019-09-11 20:12:03 +0200

    3.11 phonebook full stack

---
+++ build/precache-manifest.65d185ca7969716c3bdaab7b3d25263f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3b8fa145dd1971488656024825163f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6594633904bb1be76cb9",
+    "url": "/static/css/main.c22cdabb.chunk.css"
+  },
+  {
+    "revision": "997bafc2fd5fb0a48772",
+    "url": "/static/js/2.18074af6.chunk.js"
+  },
+  {
+    "revision": "6594633904bb1be76cb9",
+    "url": "/static/js/main.644ae755.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
