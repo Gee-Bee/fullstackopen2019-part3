16a93634a9874d55b2ab9b0bb682a8f0d4a8c2b4
Gee-Bee <greg.bunia@gmail.com>
2019-09-11 10:51:15 +0200

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
